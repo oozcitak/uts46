1c90c08f0e6eff4620bf3e61b13e2cb2d0a4294e
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-10-18 15:51:07 +0300

    Add more tests

--- test/UTS46Algorithm.test.ts
+++ test/UTS46Algorithm.test.ts
@@ -2,8 +2,25 @@ import * as uts46 from '../src'
 
 describe('UTS46Algorithm', () => {
 
-  test('toASCII', () => {
+  test('toUnicode - non-transitional', () => {
+    expect(uts46.toUnicode("fass.de", { transitionalProcessing: false })).toBe("fass.de")
+    expect(uts46.toUnicode("faß.de", { transitionalProcessing: false })).toBe("faß.de")
+    expect(uts46.toUnicode("Faß.de", { transitionalProcessing: false })).toBe("faß.de")
+    expect(uts46.toUnicode("xn--fa-hia.de", { transitionalProcessing: false })).toBe("faß.de")
+  })
+
+  test('toASCII - transitional', () => {
     expect(uts46.toASCII("fass.de")).toBe("fass.de")
+    expect(uts46.toASCII("faß.de")).toBe("fass.de")
+    expect(uts46.toASCII("Faß.de")).toBe("fass.de")
+    expect(uts46.toASCII("xn--fa-hia.de")).toBe("xn--fa-hia.de")
+  })
+
+  test('toASCII - non-transitional', () => {
+    expect(uts46.toASCII("fass.de", { transitionalProcessing: false })).toBe("fass.de")
+    expect(uts46.toASCII("faß.de", { transitionalProcessing: false })).toBe("xn--fa-hia.de")
+    expect(uts46.toASCII("Faß.de", { transitionalProcessing: false })).toBe("xn--fa-hia.de")
+    expect(uts46.toASCII("xn--fa-hia.de", { transitionalProcessing: false })).toBe("xn--fa-hia.de")
   })
 
 })
